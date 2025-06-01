42bb5a46b68fb53fb4b2331ccf9c81a26f0d5710
gajamvarsha227 <23r11a0518@gcet.edu.in>
2025-06-01 18:05:30 +0530

    update

---
+++ q17.js
@@ -0,0 +1,49 @@
+//insert documentsMore actions
+db.employees.insertOne({
+  name: "John Smith",
+  email: "john@gmail.com",
+  department: "IT",
+  salary: 1456,
+  location: ["FL", "OH"],
+  date: Date(),
+});
+
+db.employees.insertMany([
+  {
+    name: "Mike Joseph",
+    email: "mike@gmail.com",
+    department: "IT",
+    salary: 2456,
+    location: ["FL", "TX"],
+    date: Date(),
+  },
+  {
+    name: "Cathy G",
+    email: "cathy@gmail.com",
+    department: "IT",
+    salary: 3456,
+    location: ["AZ", "TX"],
+    date: Date(),
+  },
+]);
+
+db.employees.insertMany([
+  {
+    name: "Shawn Joseph",
+    email: "mike@gmail.com",
+    department: "Admin",
+    salary: 2456,
+    location: ["FL", "TX"],
+    date: Date(),
+  },
+  {
+    name: "Beth G",
+    email: "cathy@gmail.com",
+    department: "Admin",
+    salary: 3456,
+    location: ["AZ", "TX"],
+    date: Date(),
+  },
+]);
+
+db.employees.find();
